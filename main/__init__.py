a20b3dc4327a34e7814a85454c8e75a51ad4a12c
wangkingsten <wangkingsten@users.noreply.github.com>
2023-11-01 22:05:55 +0800

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -14,13 +14,13 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("API_ID", default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
+
 FORCESUB = config("FORCESUB", default=None)
 AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 with Client("pyrogram" ,api_id=API_ID ,api_hash=API_HASH, hide_password=False) as userbot:
-    SESSION = "你的pyrogram_SESSION为\n" + (pyrogram.export_session_string())
+    SESSION = userbot.export_session_string()
     print("\n正在生成你的pyrogramSESSION...\n")
     print(SESSION)
 
